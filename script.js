d09dcde025647b3302304be8271cd5865cd4275e
KermitTheSapo <rjsf06@gmail.com>
2022-06-09 06:34:07 -0300

    atividade concluida

---
+++ script.js
@@ -0,0 +1,9 @@
+let nome = "Robert"
+let numero = 18
+let anoAtual =  2022 - numero
+console.log (numero + anoAtual)
+console.log("Estou mexendo no console.log")
+let quartaFeira = false
+console.log(quartaFeira)
+console.log(typeof quartaFeira)
+
